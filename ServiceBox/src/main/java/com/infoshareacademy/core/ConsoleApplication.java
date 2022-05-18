e4df6e836065dc80bdad15ebd7be39e0526f9824
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-05-18 20:51:44 +0200

    User login

--- ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
+++ ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
@@ -1,7 +1,15 @@
 package com.infoshareacademy.core;
 
+import com.infoshareacademy.security.Security;
+import com.infoshareacademy.security.Session;
+
 public class ConsoleApplication {
     public void start() {
         System.out.println("LiveOak Team!");
+
+        Security security = new Security();
+        security.login();
+
+        System.out.println("Zalogowano jako: " + Session.getUser().getUsername());
     }
 }

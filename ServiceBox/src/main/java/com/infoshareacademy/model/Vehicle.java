8dcf47057ab15d7e42902aab03c0962238181575
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-05-21 11:09:22 +0200

    Vehicle and Adress class added

---
+++ ServiceBox/src/main/java/com/infoshareacademy/model/Vehicle.java
@@ -0,0 +1,16 @@
+package com.infoshareacademy.model;
+
+public class Vehicle {
+
+    private String vin;
+    private String platenumber;
+    private String manufacter;
+    private String carname;
+    private String cartype;
+    private String carplatform;
+    private Double enginecapacity;
+    private int productionyear;
+    private String lasttechreview;
+    private int odometerbefore;
+    private int odometerafter;
+}

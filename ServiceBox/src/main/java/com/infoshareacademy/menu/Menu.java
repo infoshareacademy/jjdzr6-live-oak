439ea5b6104b9f60764418d107453742b2a0775f
Joanna Kazun <joanna.kazun@gmail.com>
2022-05-22 12:54:30 +0200

    Add method showMenu()

---
+++ ServiceBox/src/main/java/com/infoshareacademy/menu/Menu.java
@@ -0,0 +1,16 @@
+package com.infoshareacademy.menu;
+
+public class Menu {
+
+    public void showMenu(){
+
+        System.out.println("##### Witamy w systemie ServiceBox ##### ");
+        System.out.println("##### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##### ");
+        System.out.println("1. Utwórz nowe zlecenie naprawy");
+        System.out.println("2. Wyświetl wszystkie zlecenia naprawy");
+        System.out.println("3. Wyszukaj zlecenie naprawy");
+        System.out.println("4. Zakończ");
+        System.out.println("##### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##### ");
+    }
+
+}

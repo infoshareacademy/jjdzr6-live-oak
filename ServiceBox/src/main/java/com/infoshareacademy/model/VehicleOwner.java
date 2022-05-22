4bbed3ca8b3ec234f3846ed64f1f1412d358d2c5
Joanna Kazun <joanna.kazun@gmail.com>
2022-05-22 11:40:41 +0200

    Class VehicleOwner added

---
+++ ServiceBox/src/main/java/com/infoshareacademy/model/VehicleOwner.java
@@ -0,0 +1,9 @@
+package com.infoshareacademy.model;
+
+public class VehicleOwner {
+    private Integer ownerId;
+    private String ownerName;
+    private String ownerSurname;
+    private String ownerEmail;
+    private Integer telephoneNumber;
+}

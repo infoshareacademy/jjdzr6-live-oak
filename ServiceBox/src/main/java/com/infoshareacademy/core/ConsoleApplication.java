60f29851919e2fe4151ee7d2d1bc8053e86892a0
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-05-22 15:50:59 +0200

    Rename methods, small refactoring

--- ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
+++ ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
@@ -5,7 +5,9 @@ import com.infoshareacademy.menu.Menu;
 public class ConsoleApplication {
     public void start() {
         System.out.println("LiveOak Team!");
-        System.out.println("");
+        System.out.println(""); // add empty line
+
+        // create instance of Menu class
         Menu menu = new Menu();
         menu.start();
     }
--- ServiceBox/src/main/java/com/infoshareacademy/menu/Menu.java
+++ ServiceBox/src/main/java/com/infoshareacademy/menu/Menu.java
@@ -4,9 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
-
-    private void showMenu() {
-
+    private void showMainMenu() {
         System.out.println("      Witamy w systemie ServiceBox      ");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         System.out.println("1. Utworz nowe zlecenie naprawy");
@@ -14,54 +12,52 @@ public class Menu {
         System.out.println("3. Wyszukaj zlecenie naprawy");
         System.out.println("4. Zakoncz");
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        System.out.print("Wybierz odpowiednia cyfre:  ");
+        System.out.print("Wybierz odpowiednia cyfre: ");
     }
 
-    private int getOption() {
-        int option = 0;
+    private int getOptionFromRange(int min, int max) {
+        int selectedOption = 0;
         boolean isValid = false;
-        while (isValid == false) {
+
+        // repeat until input is not valid
+        while (!isValid) {
             try {
                 Scanner scanner = new Scanner(System.in);
-                option = scanner.nextInt();
-                if (option >= 1 && option <= 4) {
+                selectedOption = scanner.nextInt();
+
+                // check if in range
+                if (selectedOption >= min && selectedOption <= max) {
                     isValid = true;
-                }else {
-                    System.out.print("Wybierz opcje z zakresu 1-4:  ");
+                } else {
+                    // out of range
+                    System.out.print("Wybierz opcje z zakresu " + min + "-" + max + ": ");
                 }
-
-
             } catch (InputMismatchException e) {
-                System.out.print("Wybrales zla opcje, sprobuj ponownie:  ");
-
+                System.out.print("Niepoprawa wartosc, sprobuj ponownie:  ");
             }
         }
 
-
-        return option;
+        return selectedOption;
     }
 
-    public void start(){
+    public void start() {
+        showMainMenu();
 
-        showMenu();
-        int option = getOption();
-        switch(option) {
+        switch (getOptionFromRange(1, 4)) {
+            case 1:
+                System.out.println("Wybrano opcje 1");
+                break;
 
-            case 1: System.out.println("Wybrano opcje 1");
-            break;
+            case 2:
+                System.out.println("Wybrano opcje 2");
+                break;
 
-            case 2: System.out.println("Wybrano opcje 2");
-            break;
+            case 3:
+                System.out.println("Wybrano opcje 3");
+                break;
 
-            case 3: System.out.println("Wybrano opcje 3");
-            break;
-
-            default: System.out.print("Zakonczono program");
-
+            default:
+                System.out.println("Zakonczono program");
         }
-
-
-
-
     }
 }

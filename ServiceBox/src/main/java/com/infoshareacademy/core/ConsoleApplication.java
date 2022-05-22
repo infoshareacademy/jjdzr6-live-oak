9f3984e192705cfcc13e40ba958d31d7cf004739
Joanna Kazun <joanna.kazun@gmail.com>
2022-05-22 13:03:48 +0200

    show.Menu

--- ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
+++ ServiceBox/src/main/java/com/infoshareacademy/core/ConsoleApplication.java
@@ -1,7 +1,12 @@
 package com.infoshareacademy.core;
 
+import com.infoshareacademy.menu.Menu;
+
 public class ConsoleApplication {
     public void start() {
         System.out.println("LiveOak Team!");
+        System.out.println("");
+        Menu menu = new Menu();
+        menu.showMenu();
     }
 }
--- ServiceBox/src/main/java/com/infoshareacademy/menu/Menu.java
+++ ServiceBox/src/main/java/com/infoshareacademy/menu/Menu.java
@@ -4,13 +4,14 @@ public class Menu {
 
     public void showMenu(){
 
-        System.out.println("##### Witamy w systemie ServiceBox ##### ");
-        System.out.println("##### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##### ");
-        System.out.println("1. Utwórz nowe zlecenie naprawy");
-        System.out.println("2. Wyświetl wszystkie zlecenia naprawy");
+        System.out.println("      Witamy w systemie ServiceBox      ");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println("1. Utworz nowe zlecenie naprawy");
+        System.out.println("2. Wyswietl wszystkie zlecenia naprawy");
         System.out.println("3. Wyszukaj zlecenie naprawy");
-        System.out.println("4. Zakończ");
-        System.out.println("##### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##### ");
+        System.out.println("4. Zakoncz");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.print("Wybierz odpowiednia cyfre:  ");
     }
 
 }

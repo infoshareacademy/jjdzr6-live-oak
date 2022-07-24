58443cd5b0440b88b0518ade44d1f7144f38675c
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-07-24 14:39:44 +0200

    Add new order state

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/entity/ServiceOrderState.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/entity/ServiceOrderState.java
@@ -2,6 +2,7 @@ package com.infoshareacademy.entity;
 
 public enum ServiceOrderState {
     CREATED("Nowe zlecenie"),
+    WAITING_FOR_PARTS("Oczekiwanie na części"),
     IN_PROGRESS("W trakcie realizacji"),
     FINISHED("Zlecenie zamknięte");
 

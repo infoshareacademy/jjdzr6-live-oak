d2922e2dbc456de1a1801b8658fe53e0853575e5
Joanna Kazun <joanna.kazun@gmail.com>
2022-07-24 13:58:09 +0200

    Code refactor - for class Mailer

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/service/Mailer.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/service/Mailer.java
@@ -1,41 +1,27 @@
 package com.infoshareacademy.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.mail.MailException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Mailer implements CommandLineRunner {
+public class Mailer {
+    @Value("${mail.from}")
+    private String from;
 
     @Autowired
     private JavaMailSender emailSender;
 
-    public void sendSimpleMessage(
+    public void sendMessage(
             String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        System.out.println("Wysylamy");
-        message.setFrom("servicebox-liveoak@wp.pl");
+        message.setFrom(from);
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
-        try {
-            emailSender.send(message);
-        } catch (MailException e) {
-            System.out.println(e.getMessage());
-            e.printStackTrace();
-                    }
-        System.out.println("OK");
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-
-        sendSimpleMessage("analysis-jk@gmail.com", "Testowy mail", "Witamy");
-
-
+        emailSender.send(message);
     }
 }
 

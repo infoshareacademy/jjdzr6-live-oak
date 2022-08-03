0ef588ceb34c7fa0f31eb8500ed62608064fe4b5
Dawid Górecki <dgorecki@amu.edu.pl>
2022-08-03 09:46:48 +0200

    Update Vehicle model

---
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/entity/Entity.java
@@ -0,0 +1,11 @@
+package com.infoshareacademy.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class Entity {
+    @Getter
+    @Setter
+    private int id;
+}

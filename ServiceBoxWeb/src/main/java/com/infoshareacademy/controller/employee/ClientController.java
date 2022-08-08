6d5b7ee02df1fa90bf530e550e25d8e34b1dcb07
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-08-08 19:06:56 +0200

    Client controller is working

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/employee/ClientController.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/employee/ClientController.java
@@ -1,4 +1,28 @@
 package com.infoshareacademy.controller.employee;
 
+import com.infoshareacademy.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("employee/")
 public class ClientController {
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    public ClientController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @GetMapping("clients")
+
+    public String getClients(Model model){
+
+        model.addAttribute("client",clientRepository.findAll());
+
+        return "employee/client-list";
+    }
 }

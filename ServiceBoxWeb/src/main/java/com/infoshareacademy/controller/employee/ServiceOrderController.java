b5ea30cd7c8d1eda55363d031107d508913cccc3
Joanna Kazun <joanna.kazun@gmail.com>
2022-08-22 07:56:41 +0200

    Added employee/add endpoint

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/employee/ServiceOrderController.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/employee/ServiceOrderController.java
@@ -1,6 +1,7 @@
 package com.infoshareacademy.controller.employee;
 
 
+import com.infoshareacademy.entity.ServiceOrder;
 import com.infoshareacademy.service.ServiceOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,17 +15,21 @@ public class ServiceOrderController {
 
     private final ServiceOrderService serviceOrderService;
 
+
     @Autowired
     public ServiceOrderController(ServiceOrderService serviceOrderService) {
         this.serviceOrderService = serviceOrderService;
     }
 
-
     @GetMapping("service-orders")
-
     public String getOrders(Model model) {
         model.addAttribute("serviceOrders", serviceOrderService.findAll());
         return "employee/service-order-list";
     }
 
+    @GetMapping("add")
+    public String getNewServiceOrder(Model model) {
+        model.addAttribute("newServiceOrder", new ServiceOrder());
+        return "employee/service-order-add";
+    }
 }

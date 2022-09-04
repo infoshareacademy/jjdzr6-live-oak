bd042d1f411e2565f81f90fbb1ea0bbc1d3783bf
Dawid Górecki <kontakt.dawidgorecki@gmail.com>
2022-09-04 18:46:12 +0200

    Add security configuration

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/security/SecurityController.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/controller/security/SecurityController.java
@@ -3,6 +3,7 @@ package com.infoshareacademy.controller.security;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
@@ -10,5 +11,12 @@ public class SecurityController {
     @GetMapping("login")
     public String loginPage() {
         return "login";
+    }
+
+    @GetMapping("/403")
+    @ResponseBody
+    public String accessDenied() {
+        // TODO: podpiąć plik html
+        return "403 Forbidden";
     }
 }

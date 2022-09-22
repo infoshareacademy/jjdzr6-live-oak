fd61eddb10d6f4004e69ada72099ac5d05bebf6f
Maciej Kusiak <mkusiak94@gmail.com>
2022-09-22 20:19:51 +0200

    Create new class VehicleDao

---
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/dao/vehicle/VehicleDao.java
@@ -0,0 +1,46 @@
+package com.infoshareacademy.dao.vehicle;
+
+import com.infoshareacademy.dao.Dao;
+import com.infoshareacademy.entity.vehicle.Vehicle;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class VehicleDao implements Dao<Vehicle> {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public Vehicle find(Long id) {
+        return entityManager.find(Vehicle.class, id);
+    }
+
+    @Override
+    public List<Vehicle> findAll() {
+        TypedQuery<Vehicle> query = entityManager.createQuery(
+                "SELECT v from Vehicle v ", Vehicle.class);
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Vehicle vehicle) {
+        entityManager.persist(vehicle);
+    }
+
+    @Override
+    public Vehicle update(Vehicle vehicle) {
+        entityManager.merge(vehicle);
+        return vehicle;
+    }
+
+    @Override
+    public void delete(Vehicle vehicle) {
+        if (vehicle != null) {
+            entityManager.remove(vehicle);
+        }
+    }
+}

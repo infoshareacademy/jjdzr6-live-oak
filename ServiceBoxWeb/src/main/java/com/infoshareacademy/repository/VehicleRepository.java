5e1d62401a15e3beabd8a67fee06b2b1c5402db7
Przemysław Bukowski <bukowski.przemyslaw.1988@gmail.com>
2022-11-06 13:58:39 +0100

    Peagable and sort added to vehicle-list

--- ServiceBoxWeb/src/main/java/com/infoshareacademy/repository/VehicleRepository.java
+++ ServiceBoxWeb/src/main/java/com/infoshareacademy/repository/VehicleRepository.java
@@ -3,6 +3,8 @@ package com.infoshareacademy.repository;
 import com.infoshareacademy.entity.vehicle.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +13,7 @@ import java.util.Optional;
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
     @Query("SELECT v FROM Vehicle v WHERE LOWER(v.plateNumber) LIKE LOWER(concat('%',:query,'%'))")
-    List<Vehicle> findByQuery(String query);
+    Page<Vehicle> findByQuery(String query, Pageable pageable);
 
     Optional<Vehicle> findByPlateNumber(String plateNumber);
 
